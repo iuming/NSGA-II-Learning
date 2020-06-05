d6779e28a4dc5a0f338ab8cd59a9564663b61708
刘铭 <37214643+iuming@users.noreply.github.com>
2020-06-05 15:09:04 +0800

    Update report.c

--- report.c
+++ report.c
@@ -8,7 +8,7 @@
 # include "rand.h"
 
 /* Function to print the information of a population in a file */
-// report_pop   将种群中所有个体的   目标函数值， 限制条件值， 编码值  打印出来
+/* report_pop   将种群中所有个体的   目标函数值， 限制条件值， 编码值  打印出来 */
 void report_pop (population *pop, FILE *fpt)
 {
     int i, j, k;
@@ -54,7 +54,7 @@ void report_feasible (population *pop, FILE *fpt)
     int i, j, k;
     for (i=0; i<popsize; i++)
     {
-        // report_pop   种群中的非支配个体并且限制条件总和为0   （constr_violation == 0.0）的个体的   目标函数值， 限制条件值， 编码值  打印出来。
+        /* report_pop   种群中的非支配个体并且限制条件总和为0   （constr_violation == 0.0）的个体的   目标函数值， 限制条件值， 编码值  打印出来。 */
         if (pop->ind[i].constr_violation == 0.0 && pop->ind[i].rank==1)
         {
             for (j=0; j<nobj; j++)
